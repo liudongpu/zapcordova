b352217eb5b86c3038515372fbbf7abab83a4dda
srnpr <srnpr@srnprmacmini>
2014-05-27 23:17:32 +0800

    a

--- zapcordova/platforms/android/src/com/srnpr/zapmobile/zapmobile.java
+++ zapcordova/platforms/android/src/com/srnpr/zapmobile/zapmobile.java
@@ -32,10 +32,7 @@ public class zapmobile extends CordovaActivity
         
         
         // Set by <content src="index.html" /> in config.xml
-        //super.loadUrl(Config.getStartUrl());
-        
-        super.loadUrl("http://yinxl.com/mobile/index");
-        
+        super.loadUrl(Config.getStartUrl());
         //super.loadUrl("file:///android_asset/www/index.html");
     }
 }
