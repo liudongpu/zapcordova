6ceb5f2d977b4f2526d9c432fa21c2726c7001f5
srnpr <srnpr@srnprmacmini>
2014-06-29 22:48:54 +0800

    a

--- zapcordova/platforms/android/assets/www/cordova_plugins.js
+++ zapcordova/platforms/android/assets/www/cordova_plugins.js
@@ -27,12 +27,20 @@ module.exports = [
         "clobbers": [
             "CameraPopoverHandle"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.camera": "0.2.9"
+    "org.apache.cordova.camera": "0.2.9",
+    "org.apache.cordova.splashscreen": "0.3.1"
 }
 // BOTTOM OF METADATA
 });
